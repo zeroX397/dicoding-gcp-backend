1265dc88c000ecdef81868bf03cc6a797a3de745
Christopher Pengalilla <christopher.pengalilla@gmail.com>
2024-11-11 11:37:39 +0700

    Response Body

---
+++ server7.js
@@ -0,0 +1,62 @@
+const requestListener = (request, response) => {
+    response.setHeader('Content-Type', 'application/json');
+    response.setHeader('X-Powered-By', 'NodeJS');
+
+    const { method, url } = request;
+
+    if (url === '/') {
+        if (method === 'GET') {
+            response.statusCode = 200;
+            response.end(JSON.stringify({
+                message: 'Ini adalah homepage',
+            }));
+        } else {
+            response.statusCode = 400;
+            response.end(JSON.stringify({
+                message: `Halaman tidak dapat diakses dengan ${method} request`,
+            }));
+        }
+    } else if (url === '/about') {
+        if (method === 'GET') {
+            response.statusCode = 200;
+            response.end(JSON.stringify({
+                message: 'Halo! Ini adalah halaman about',
+            }));
+        } else if (method === 'POST') {
+            let body = [];
+
+            request.on('data', (chunk) => {
+                body.push(chunk);
+            });
+
+            request.on('end', () => {
+                body = Buffer.concat(body).toString();
+                const { name } = JSON.parse(body);
+                response.statusCode = 200;
+                response.end(JSON.stringify({
+                    message: `Halo, ${name}! Ini adalah halaman about`,
+                }));
+            });
+        } else {
+            response.statusCode = 400;
+            response.end(JSON.stringify({
+                message: `Halaman tidak dapat diakses menggunakan ${method}, request`
+            }));
+        }
+    } else {
+        response.statusCode = 404;
+        response.end(JSON.stringify({
+            message: 'Halaman tidak ditemukan!',
+        }));
+    }
+};
+
+/*
+Run following command on CLI:
+curl -X GET http://localhost:5000/
+// output: {"message":"Ini adalah homepage"}
+curl -X GET http://localhost:5000/about
+// output: {"message":"Halo! ini adalah halaman about"}
+curl -X DELETE http://localhost:5000/
+// output: {"message":"Halaman tidak dapat diakses dengan DELETE request"}
+*/
